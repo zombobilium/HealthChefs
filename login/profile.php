dd1639a4d40e06498dcfea02f05a8d9c50d404e2
armysick <uh4v3b33nh4ck3d@gmail.com>
2016-11-15 20:48:37 +0000

    Some complex Queries + filled up the Basic Info table

--- login/profile.php
+++ login/profile.php
@@ -22,6 +22,8 @@
   <?php 
 		include_once("connection.php");
 		session_start();
+		
+		// Row 1 of Table 1
 		$stmt =$db->prepare('SELECT COUNT(*) As facil FROM "public"."Login" WHERE data BETWEEN now()- INTERVAL \'7 day\' AND now() ;');  /* Starts from the past to the future*/
 		$stmt->execute();
 		$lastweek = $stmt->fetch();
@@ -33,6 +35,51 @@
 		$stmt3 =$db->prepare('SELECT COUNT(*) As facil FROM "public"."Login"');
 		$stmt3->execute();
 		$always = $stmt3->fetch();
+		
+		// Row 2 of Table 1
+		// SELECT column where each user has oldest login in last week 
+		
+		$stmt4 =$db->prepare('SELECT COUNT(*) As facil FROM(
+		SELECT mt.* FROM "public"."Login" As mt INNER JOIN
+    (
+        SELECT "public"."Login"."idUser", MIN(data) As MinDate
+        FROM "public"."Login"
+        GROUP BY "public"."Login"."idUser"
+    ) t ON "mt"."idUser" = "t"."idUser" AND mt.data = t.MinDate
+	) As mmt
+	WHERE data BETWEEN now()- INTERVAL \'7 day\' AND now()');
+		$stmt4->execute();
+		$lastweekregs = $stmt4->fetch();
+		
+		
+		// SELECT column where each user has oldest login in last month
+		
+		
+		$stmt5 =$db->prepare('SELECT COUNT(*) As facil FROM(
+		SELECT mt.* FROM "public"."Login" As mt INNER JOIN
+    (
+        SELECT "public"."Login"."idUser", MIN(data) As MinDate
+        FROM "public"."Login"
+        GROUP BY "public"."Login"."idUser"
+    ) t ON "mt"."idUser" = "t"."idUser" AND mt.data = t.MinDate
+	) As mmt
+	WHERE data BETWEEN now()- INTERVAL \'31 day\' AND now()');
+		$stmt5->execute();
+		$lastmonthregs = $stmt5->fetch();
+		
+		// SELECT column where each user has oldest login SINCE Always
+		
+		$stmt6 =$db->prepare('SELECT COUNT(*) As facil FROM(
+		SELECT mt.* FROM "public"."Login" As mt INNER JOIN
+    (
+        SELECT "public"."Login"."idUser", MIN(data) As MinDate
+        FROM "public"."Login"
+        GROUP BY "public"."Login"."idUser"
+    ) t ON "mt"."idUser" = "t"."idUser" AND mt.data = t.MinDate
+	) As mmt');
+		$stmt6->execute();
+		$alwaysregs = $stmt6->fetch();
+		
                         
     ?>
 	<h1><img src="css/basictitle.png" width="30%" height="30%" /></h1>
@@ -48,40 +95,33 @@
 	  </tr>
 	  
 	  <tr>
-		<!--<td><input type="radio"/></td>-->
 		<td><h4>Number of Logins:</h4></td>
-		<!-- insert php -->
 		<td><?php echo $lastweek['facil'];  ?></td>
 		<td><?php echo $lastmonth['facil']; ?></td>
 		<td><?php echo $always['facil'];    ?></td>
 	  </tr>
 	  
-	  <tr>
-		<!--<td><input type="radio"/></td>-->
+	  <!--<tr>
 		<td><h4>Number of hours played: </h4></td>
-		<!-- insert php -->
 		<td>Foo</td>
 		<td>01/01/1978</td>
 		<td>Spouse</td>
-	  </tr>
+	  </tr>-->
 	  
 	  <tr>
-		<!--<td><input type="radio"/></td>-->
 		<td><h4>Number of [Registered] Players: </h4></td>
-		<!-- insert php -->
-		<td>Foo</td>
-		<td>01/01/1994</td>
-		<td>Son</td>
+		<td><?php echo $lastweekregs['facil'];  ?></td>
+		<td><?php echo $lastmonthregs['facil'];  ?></td>
+		<td><?php echo $alwaysregs['facil'];  ?></td>
 	  </tr>
 	  
+	  <!--
 	  <tr>
-		<!--<td><input type="radio"/></td>-->
 		<td><h4>Number of average hours played per login: </h4></td>
-		<!-- insert php with aritmetics -->
 		<td>Foo</td>
 		<td>01/01/1992</td>
 		<td>Daughter</td>
-	  </tr>
+	  </tr>-->
   
 </table>
 

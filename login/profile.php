5db52340fcbf593f2b0d49c540eed1b59bec31d6
armysick <uh4v3b33nh4ck3d@gmail.com>
2016-11-15 12:40:47 +0000

    Added Postlogin Landpage + table with CSS

---
+++ login/profile.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html lang="en">
+
+	<head>
+    <meta charset="utf-8">
+
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <!-- Use title if it's in the page YAML frontmatter -->
+    <title>HealthChef Data View Page</title>
+
+    <link rel="stylesheet" href="css/profile.css">
+
+
+  </head>
+
+  <body>
+  <!--   Table credits on  http://codepen.io/jordyvanraaij/pen/jlAqp -->
+	<h1>Basic Information Table</h1>
+
+	<table>
+	  
+	  <tr>
+		<!--<th>Main driver</th>-->
+		<th data-th="Driver details"><span>First name</span></th>
+		<th>Surname</th>
+		<th>Date of birth</th>
+		<th>Relationship</th>
+	  </tr>
+	  
+	  <tr>
+		<!--<td><input type="radio"/></td>-->
+		<td>Steve</td>
+		<td>Foo</td>
+		<td>01/01/1978</td>
+		<td>Policyholder</td>
+	  </tr>
+	  
+	  <tr>
+		<!--<td><input type="radio"/></td>-->
+		<td>Steffie</td>
+		<td>Foo</td>
+		<td>01/01/1978</td>
+		<td>Spouse</td>
+	  </tr>
+	  
+	  <tr>
+		<!--<td><input type="radio"/></td>-->
+		<td>Stan</td>
+		<td>Foo</td>
+		<td>01/01/1994</td>
+		<td>Son</td>
+	  </tr>
+	  
+	  <tr>
+		<!--<td><input type="radio"/></td>-->
+		<td>Stella</td>
+		<td>Foo</td>
+		<td>01/01/1992</td>
+		<td>Daughter</td>
+	  </tr>
+  
+</table>
+
+	</body>
+</html>

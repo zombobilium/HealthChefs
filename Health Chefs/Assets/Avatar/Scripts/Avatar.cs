a024875e6aa2ddd5b1308ce3b49dab2a5013b3d5
Eduardo Reis <up201303832@gcloud.fe.up.pt>
2016-12-03 23:35:49 +0000

    Avatar customization working

---
+++ Health Chefs/Assets/Avatar/Scripts/Avatar.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Avatar : MonoBehaviour {
+
+	public Image head;
+	public Image torso;
+	public Image legs;
+	public Button male;
+	public Button female;
+	public Button white;
+	public Button black;
+
+	void Start () 
+	{
+		head.sprite = AvatarSelection.selectCurrentHead ();
+		torso.sprite = AvatarSelection.selectCurrentTorso ();
+		legs.sprite = AvatarSelection.selectCurrentLegs ();
+	}
+
+	public void selectHeadAfter()
+	{
+		head.sprite = AvatarSelection.selectHeadAfter();
+	}
+
+	public void selectHeadBefore()
+	{
+		head.sprite = AvatarSelection.selectHeadBefore();
+	}
+
+	public void selectTorsoAfter()
+	{
+		torso.sprite = AvatarSelection.selectTorsoAfter ();
+	}
+
+	public void selectTorsoBefore()
+	{
+		torso.sprite = AvatarSelection.selectTorsoBefore ();
+	}
+
+	public void selectLegsAfter()
+	{
+		legs.sprite = AvatarSelection.selectLegsAfter ();
+	}
+
+	public void selectLegsBefore()
+	{
+		legs.sprite = AvatarSelection.selectLegsBefore ();
+	}
+
+	public void selectMaleGender()
+	{
+		AvatarSelection.setMale ();
+		head.sprite = AvatarSelection.selectCurrentHead ();
+		torso.sprite = AvatarSelection.selectCurrentTorso ();
+		legs.sprite = AvatarSelection.selectCurrentLegs ();
+
+		male.interactable = false;
+		female.interactable = true;
+	}
+
+	public void selectFemaleGender()
+	{
+		AvatarSelection.setFemale ();
+		head.sprite = AvatarSelection.selectCurrentHead ();
+		torso.sprite = AvatarSelection.selectCurrentTorso ();
+		legs.sprite = AvatarSelection.selectCurrentLegs ();
+
+		female.interactable = false;
+		male.interactable = true;
+	}
+
+	public void selectWhiteSkin()
+	{
+		AvatarSelection.setWhiteSkin ();
+		head.sprite = AvatarSelection.selectCurrentHead ();
+		torso.sprite = AvatarSelection.selectCurrentTorso ();
+		legs.sprite = AvatarSelection.selectCurrentLegs ();
+
+		white.interactable = false;
+		black.interactable = true;
+	}
+
+	public void selectBlackSkin()
+	{
+		AvatarSelection.setBlackSkin ();
+		head.sprite = AvatarSelection.selectCurrentHead ();
+		torso.sprite = AvatarSelection.selectCurrentTorso ();
+		legs.sprite = AvatarSelection.selectCurrentLegs ();
+
+		black.interactable = false;
+		white.interactable = true;
+	}
+}
